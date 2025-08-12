94c3fc46aa17a43f85757fa8ec8dfe90b5974738
subhrank09 <167698982+subhrank09@users.noreply.github.com>
2025-08-12 21:42:12 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000", // use proxy for local dev
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
   withCredentials: true,
 });
 
