512e1d1aecdcce537b580660bd6458a641d800c8
subhrank09 <subhrank42official@gmail.com>
2025-08-15 12:41:34 +0530

    change auth api base url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
+  baseURL: `${process.env.REACT_APP_API_URL}/api` || "http://localhost:5000/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
